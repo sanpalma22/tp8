105ec76c938bcd53c7f0630822ea9144f7132a45
SANTIAGO ALEJANDRO PALMA <47490026@ort>
2023-09-26 08:37:02 -0300

    v1

---
+++ ejercicios.js
@@ -0,0 +1,50 @@
+function CalcularEdad() {
+    const fechaString = document.getElementById("fechaNac").value
+    const fechaDate = Date.parse(fechaString)
+    let hoy = Date.now()
+
+    let años = Math.abs(Date.now() - fechaDate);
+    años = años / (1000 * 3600 * 24)
+    let respuesta = document.getElementById("resultado")
+    respuesta.innerHTML = años
+}
+
+
+let frutas = ["Manzana", "Banana", "Naranja", "Uva", "Pera", "Mango", "Kiwi", "Fresa", "Piña", "Cereza"]
+for (const fruta of frutas) {
+    console.log(fruta)
+}
+function BuscarFruta() {
+    const frutaBuscada = document.getElementById("fruta").value
+
+    if (frutas.indexOf(frutaBuscada) >= 0) respuesta.innerHTML = "Sí, tenemos " + frutaBuscada
+    else respuesta.innerHTML = "No, no tenemos " + frutaBuscada
+}
+
+function ComparacionDatos() {
+    if (10 == '10') console.log("Son iguales")
+    if (10 === '10') console.log("Son iguales")
+    else console.log("Son distintos")
+}
+
+function yoObjeto() {
+    const ciudad = {
+        nombre: "Buenos Aires",
+        fechaFundacion: "1980",
+        poblacion: "5399",
+        extencion: "75894km"
+    }
+    for (const propiedad in ciudad) {
+        console.log(`${propiedad} : ${ciudad[propiedad]}`);
+    }
+}
+
+
+function dobleElemento() {
+    let numeros = [2, 4, 7]
+    let numDuplicados = []
+    for (const num of numeros) {
+        numDuplicados.push(num * 2)
+    }
+    console.log(numDuplicados)
+}
