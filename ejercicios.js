3e124cfe07862d317585e231829663e0a825c53c
SANTIAGO ALEJANDRO PALMA <47490026@ort>
2023-09-28 15:55:57 -0300

    v3

--- ejercicios.js
+++ ejercicios.js
@@ -15,10 +15,10 @@ for (const fruta of frutas) {
     console.log(fruta)
 }
 function BuscarFruta() {
-    const frutaBuscada = document.getElementById("fruta").value
-
-    if (frutas.indexOf(frutaBuscada) >= 0) respuesta.innerHTML = "Sí, tenemos " + frutaBuscada
-    else respuesta.innerHTML = "No, no tenemos " + frutaBuscada
+    let frutaBuscada = document.getElementById('fruta').value
+    let respuesta = document.getElementById('resultado')
+    if (frutas.indexOf(frutaBuscada) >= 0) respuesta.innerHTML = `Si, tenemos  ${frutaBuscada}`
+    else respuesta.innerHTML = `No, no tenemos  ${frutaBuscada}`
 }
 
 function ComparacionDatos() {
@@ -49,23 +49,68 @@ function dobleElemento() {
     console.log(numDuplicados)
 }
 
-function piramide(){
+function piramide() {
     let numBase = prompt("Ingrese la cantidad de asteriscos que tiene la base")
-    numBase= parseInt(numBase)
+    numBase = parseInt(numBase)
     for (let i = 1; i <= numBase; i++) {
         console.log("*".repeat(i))
     }
-    
+
+    numBase = prompt("Ingrese la cantidad de asteriscos que tiene la base")
+    numBase = parseInt(numBase)
+    let base = "-".repeat(numBase)
     for (let i = 1; i <= numBase; i++) {
-        
+
     }
 }
 
-function nombresA(){
-    let nomrbes = prompt("Ingrese una lista de nombres separados por coma")
-    let nombresConA=[]
-    for (const nombre of nombres){
-        if(nombre)
+function nombresA() {
+    let ingreso = prompt("Ingrese una lista de nombres separados por coma")
+
+    let nombres = ingreso.split(',')
+    let nombresConA = nombres.filter(nombre => nombre.startsWith("a"))
+
+    console.log(nombres)
+    console.log(nombresConA)
+}
+
+function reemplazarPalabra() {
+    let ingreso = prompt("Ingrese una lcadena de texto")
+    let palabraAReemplazar = prompt("Ingrese una palabra que deseas reemplazar")
+    let palabraNueva = prompt("Ingrese la nueva palabra")
+
+    let fraseNueva = document.getElementById('resultado')
+    fraseNueva.innerHTML = ingreso.replace(palabraAReemplazar, palabraNueva)
+}
+
+function cortarTexto() {
+    let ingreso = prompt("Ingrese una lcadena de texto")
+    let numero = prompt("Ingrese un numero para cortar el texto anterior")
+
+    let fraseCortada = document.getElementById('resultado')
+    fraseCortada.innerHTML = ingreso.slice(0, numero)
+}
+
+function stringConSeparador() {
+    let ingreso = prompt("Ingrese una cadena de elementos separados por una coma")
+
+    let elementosSinComa = document.getElementById('resultado')
+    elementosSinComa.innerHTML = ingreso.replaceAll(',', '-')
+}
+
+function calculadoraDeRecaudacion() {
+    let ingreso = prompt("Ingrese un texto de los productos con sus perspectivos precios")
+    let pedidos = ingreso.split(',')
+    console.log(pedidos)
+    let precios=[]
+    for (let i = 0; i < pedidos.length; i++) {
+        precios.push(pedidos[i].split(':').filter(item => !isNaN(item)))
     }
+    console.log(precios)
+    let total = 0
+    for (let i = 0; i < precios.length; i++) {
+        total += precios[i]
+    }
+    console.log(total)
 
 }
